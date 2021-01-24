a9027303c65fbfd393a986aa32acdb4299081734
ariel <ariel@elucidate.co>
2021-01-24 15:33:27 +0100

    done for main-script

--- main-script.js
+++ main-script.js
@@ -4,7 +4,7 @@ function firstPart () {
 }
 
 function secondPart () {
-    const sentenceOptions = ['you will meet', 'you closest friend will eat', 'your mom will find'];
+    const sentenceOptions = ['you will meet', 'your closest friend will eat', 'your mom will find'];
     return sentenceOptions[Math.floor(Math.random() * 3)];
 }
 
@@ -12,3 +12,9 @@ function thirdPart () {
     const sentenceOptions = ['your biggest fear', 'your first love', 'your secret stash'];
     return sentenceOptions[Math.floor(Math.random() * 3)];
 }
+
+const mixedMessagesGenerator = () => {
+    return `Your destiny is:\n${firstPart()} ${secondPart()} ${thirdPart()}`;
+}
+
+console.log(mixedMessagesGenerator());
